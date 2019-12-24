cbee8c8c64a9f28422694a9ef748587ed9627ff0
Sandra Trošina <pr.mish@gmail.com>
2019-12-24 21:45:35 +0200

    Pievienoti komentaaru un attelu ID
    Pievienotas metodes, kas dod atlasa un ielādē attēlus, komentārus pēc ID;  pievienotas metodes, kas ģenerē ID attēliem un komentāriem. Pievienoti attēli komentāru rediģēšanai, dzēšanai, pievienošanai. Nodzēsti nevajadzīgie faili.

--- modules/commethods.py
+++ modules/commethods.py
@@ -1,5 +1,5 @@
 #Funkcija, kas meklē visus vienas bildes komentārus pēc bildes nosaukuma
-def searchAllComments(picName):
+def searchAllComments(picID):
   pictureComments = []
   allComments = []
   filepath = 'static/comments.bin'
@@ -13,15 +13,26 @@ def searchAllComments(picName):
   fp.close()
   for fullInfoLine in allComments:
     temp = fullInfoLine.split(';')
-    if temp[0] == picName:
-      pictureComments.append(temp[1])
+    if temp[0] == picID:
+      pictureComments.append(temp[2])
   return pictureComments
 
+#Funkcija, kas saskaita, cik komentāru ir konkrētam attēlam comments.bin un atgriež konkrētu skaitli+1, 
+#lai varētu piešķirt ID nākamajam jaunajam komentāram
+def generateNextCommID(picID):
+  nextID = 0
+  allPicComments = searchAllComments(picID)
+  for comment in allPicComments:
+    if comment != '':
+      nextID +=1
+  return nextID+1
+
 #Funkcija, kas pievieno konkrētam attēlam jaunu komentāru
-def addNewComment(picName,comment):
+def addNewComment(picID,comment):
   try:
     with open('static/comments.bin',mode='a') as commentFile:
-      text = '\n'+ picName + ';' + comment
+      nextCommID = generateNextCommID(picID)
+      text = picID + ';' + nextCommID + ';' + comment + '\n'
       commentFile.write(text)
       commentFile.close()
   except:
--- modules/picmethods.py
+++ modules/picmethods.py
@@ -1,4 +1,5 @@
-#Funkcija, kas ielādē visus attēlu nosaukumus vienā sarakstā pictures un atgriež to, lai varētu no tā pēc nosaukumiem ielādēt attēlus html lapā
+#Funkcija, kas ielādē visus attēlu nosaukumus vienā sarakstā pictures un atgriež to, 
+#lai varētu no tā pēc nosaukumiem ielādēt attēlus html lapā
 def loadAllPictures():
   filepath = 'static/pictures.bin'
   pictures=[]
@@ -6,7 +7,9 @@ def loadAllPictures():
     line = fp.readline()
     cnt = 1
     while line:
-      pictures.append(line.strip())
+      if line!='':
+        fullPicInfo = line.strip().split(';')
+        pictures.append(fullPicInfo[1])
       line = fp.readline()
       cnt += 1
   fp.close()
@@ -21,11 +24,22 @@ def checkIfPictureExists(pictureName):
       exists = True
   return exists
 
+#Funkcija, kas saskaita, cik attēlu ir pictures.bin un atgriež konkrētu skaitli+1, 
+#lai varētu piešķirt ID nākamajam jaunajam attēlam
+def generateNextPicID():
+  nextID = 0
+  allPictures = loadAllPictures()
+  for picture in allPictures:
+    if picture != '':
+      nextID +=1
+  return nextID+1
+
 #Funkcija, kas pievieno jaunu attēlu datnē pictures.bin
 def addNewPicture(pictureName):
   try:
     with open('static/pictures.bin',mode='a') as pictureFile:
-      text = '\n'+ pictureName
+      picID = generateNextPicID
+      text = picID + ';' + pictureName + '\n'
       pictureFile.write(text)
       pictureFile.close()
   except:
@@ -33,3 +47,20 @@ def addNewPicture(pictureName):
   finally:
     print("All ok")
   return 0
+
+#Funkcija, kas pēc attēla nosaukuma atgriež attēla ID no pictures.bin
+def findPictureIdByName(picName):
+  picID = 0
+  filepath = 'static/pictures.bin'
+  with open(filepath) as fp:
+    line = fp.readline()
+    cnt = 1
+    while line:
+      if line!='':
+        fullPicInfo = line.strip().split(';')
+        if fullPicInfo[1] == picName:
+          picID = fullPicInfo[0]
+      line = fp.readline()
+      cnt += 1
+  fp.close()
+  return picID
