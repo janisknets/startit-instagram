f9554fc0b8a79586aae261c112e364f7aaf45cf6
Sandra Trošina <pr.mish@gmail.com>
2020-02-01 10:50:13 +0200

    Čata komandas
    Pievienota čata komanda /away, darbojas arī čata komanda /nick

--- static/chats.js
+++ static/chats.js
@@ -20,29 +20,58 @@ function raadiChatuVienkaarshi(dati){
 async function suutiZinju(){
     let zinjasElements = document.getElementById("zinja");
     let zinja = zinjasElements.value;
+
+    //šajā if-ā pārbaudām, vai pirmais ziņas simbols ir /, kas apzīmē komandas sākumu
     if (zinja[0] === '/'){
-        //Nolasām no html lapas veco niku un saglabājam mainīgajā vecsVards
-        let vecsVards = document.getElementById('vards').innerHTML;
-        //Nolasām no ziņas jauno niku un saglabājam mainīgajā jaunsVards
-        //Nogriežam no visas sūtāmās ziņas nost sākumu /nick 
-        let jaunsVards = zinja.substring(6,zinja.length);
-        //Ierakstām jauno vārdu cepumā - atrodam cepumu 'lietotajs'
-        document.cookie = "lietotajs=" + jaunsVards + "; expires=Thu, 26 Nov 2020 12:00:00 UTC; path=/";
-        //Tagad jānomaina lietotājvārds pašā lapā 
-        document.getElementById("vards").innerHTML = jaunsVards;
-        //Un jānosūta visiem čatā ziņa par to, ka lietotājs nomainījis lietotājvārdu
-        zinjasElements.value = "";
-        zinja = "Lietotājs " + vecsVards + " tagad zināms kā " + jaunsVards;
-        const atbilde = await fetch("/chats/suuti",{
-            method: "POST",
-            headers:
-            {
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify({"chats":zinja})
-        });
-        const datuObjekts = await atbilde.json();
-        raadiChataRindas(datuObjekts)
+        if (zinja.substring(1,5)=='nick'){
+            //Ja tiek dota komanda /nick un jaunais lietotājvārds
+
+            //Nolasām no html lapas veco niku un saglabājam mainīgajā vecsVards
+            let vecsVards = document.getElementById('vards').innerHTML;
+            //Nolasām no ziņas jauno niku un saglabājam mainīgajā jaunsVards
+            //Nogriežam no visas sūtāmās ziņas nost sākumu /nick 
+            let jaunsVards = zinja.substring(6,zinja.length);
+            //Ierakstām jauno vārdu cepumā - atrodam cepumu 'lietotajs'
+            document.cookie = "lietotajs=" + jaunsVards + "; expires=Thu, 26 Nov 2020 12:00:00 UTC; path=/";
+            //Tagad jānomaina lietotājvārds pašā lapā 
+            document.getElementById("vards").innerHTML = jaunsVards;
+            //Un jānosūta visiem čatā ziņa par to, ka lietotājs nomainījis lietotājvārdu
+            zinjasElements.value = "";
+            zinja = "Lietotājs " + vecsVards + " tagad zināms kā " + jaunsVards;
+            const atbilde = await fetch("/chats/suuti",{
+                method: "POST",
+                headers:
+                {
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify({"chats":zinja})
+            });
+            const datuObjekts = await atbilde.json();
+            raadiChataRindas(datuObjekts)
+        }
+        else if (zinja.substring(1,5)=='away'){
+            //Ja tiek dota komanda /away un paziņojums
+            let cepumsLietotajs = getCookie('lietotajs');
+            //Nogriežam no visas sūtāmās ziņas nost sākumu /away 
+            let awayZinja = zinja.substring(6,zinja.length);
+            //Sastādām paziņojumu, ka lietotājs ir away + ziņa un parādām kopējā čatā
+            zinja = "Lietotājs " + cepumsLietotajs + " ir prom, jo " + "\'" + awayZinja + "\'";
+            zinjasElements.value = "";
+            const atbilde = await fetch("/chats/suuti",{
+                method: "POST",
+                headers:
+                {
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify({"chats":zinja})
+            });
+            const datuObjekts = await atbilde.json();
+            raadiChataRindas(datuObjekts)
+        }
+        else if (zinja.substring(1,3)=='me'){
+            //Ja tiek dota komanda /me un paziņojums
+            //Te būs sarežģītāk, jo nāksies mainīt arī ziņas dizainu...
+        }
     }
     else{
         zinjasElements.value = "";
@@ -91,4 +120,20 @@ function raadiChataRindas(dati) {
     newLI.appendChild(newDiv); 
     newDiv.appendChild(newContent); 
     return newLI;
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
   }
